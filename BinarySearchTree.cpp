c449284b0d482406879b00a062e6af1634d50c7e
lulultfh <punyalulu05@gmail.com>
2024-06-13 15:12:59 +0700

    add member constructor untuk class node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -7,4 +7,11 @@ public:
     string info;
     Node* leftchild;
     Node* rightchild;
+
+    //constructor for the node class
+    Node(string i, Node* l, Node* r) {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
