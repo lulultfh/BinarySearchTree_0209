368a31243695e6e84a3fab7b3f09772be0876971
lulultfh <punyalulu05@gmail.com>
2024-06-13 15:54:29 +0700

    add member procedure void postorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -91,4 +91,19 @@ public:
             preorder(ptr->rightchild); //RIGHT
         }
     }
+
+    
+    void postorder(Node* ptr)
+    {
+        if(ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL) {
+            postorder(ptr->leftchild); //LEFT
+            postorder(ptr->rightchild); //RIGHT
+            cout << ptr->info << " "; //ROOT
+        }
+    }
 };
