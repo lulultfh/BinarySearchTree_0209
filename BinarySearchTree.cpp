74ddc27838c1207f3eb8a7785b88726c35478f98
lulultfh <punyalulu05@gmail.com>
2024-06-13 15:22:22 +0700

    add step 1

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -26,6 +26,6 @@ public:
 
     void insert(string element) //insert a node in the binary search tree
     {
-        
+        Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
     }
 };
