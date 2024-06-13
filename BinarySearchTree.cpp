d0437be4f1f728fa160fc02e08a2b3d4f81d03c3
lulultfh <punyalulu05@gmail.com>
2024-06-13 15:24:14 +0700

    add step 3

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -28,5 +28,7 @@ public:
     {
         Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
         newNode->info = element; //assign value to the data of the new node
+        newNode->leftchild = nullptr; // make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; // make the right child of the new node point to NULL
     }
 };
